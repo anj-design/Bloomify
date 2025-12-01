c3ef7336e94cf22b6ad511fc8399c2ed7117a1f9
GeorGiee_ <georgiebognalbal0@gmail.com>
2025-12-01 21:25:10 +0800

    - InputSimulator

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WindowsInput;
+using WindowsInput.Native;
+
+namespace Bloomify_2._0
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            InputSimulator simulator = new InputSimulator();
+
+            simulator.Keyboard.KeyPress(VirtualKeyCode.F11);
+            simulator.Keyboard.ModifiedKeyStroke(VirtualKeyCode.CONTROL, VirtualKeyCode.SUBTRACT);
+            Thread.Sleep(100);
+            Background.Show();
+        }
+
+        public static void MainMenu()
+        {
+            Banner.Show();   // show your ASCII art first
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey(true);
+
+            //Menu.Show();     // then launch the interactive menu
+        }
+
+
+    }
+
+}
--- TitleScreen.cs
+++ TitleScreen.cs
@@ -5,23 +5,20 @@ using System.Media;
 using System.Text;
 using System.Threading;
 
-namespace eggnog
+public class Background
 {
-    internal class Background
+    public static void Show()
     {
-        public void Show()
+
+
+        SoundPlayer player = new SoundPlayer
         {
+            SoundLocation = "bgmusic1.wav"
+        };
+        player.Load();
+        player.PlayLooping();
 
 
-            SoundPlayer player = new SoundPlayer
-            {
-                SoundLocation = "bgmusic1.wav"
-            };
-            player.Load();
-            player.PlayLooping();
-
-            Console.ReadKey();
-   
         string asciiBloom = @"
 
 
@@ -63,24 +60,23 @@ namespace eggnog
         // Print Bloom once and keep it
         Console.WriteLine(asciiBloom + asciiEnter);
 
-            while (true)
-            {
-                // blinking logic for Enter art here...
+        while (true)
+        {
+            // blinking logic for Enter art here...
 
-                if (Console.KeyAvailable)
+            if (Console.KeyAvailable)
+            {
+                var key = Console.ReadKey(true);
+                if (key.Key == ConsoleKey.Enter)
                 {
-                    var key = Console.ReadKey(true);
-                    if (key.Key == ConsoleKey.Enter)
-                    {
-                        break; // exit the while loop
-                    }
+                    break; // exit the while loop
                 }
             }
+        }
 
-            Console.Clear();
-            Console.BackgroundColor = ConsoleColor.White;
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine(@"
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine(@"
           /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\  
          \       _ _  __             _ _  __            _ _  __              _ _  __              _ _  __               _ _  __             _ _  __  / 
          /      ( | )/_/            ( | )/_/           ( | )/_/             ( | )/_/             ( | )/_/              ( | )/_/            ( | )/_/  \ 
@@ -118,19 +114,7 @@ namespace eggnog
          /  \_\(_|_)           \_\(_|_)            \_\(_|_)              \_\(_|_)            \_\(_|_)              \_\(_|_)             \_\(_|_)     \ 
          \                                                                                                                                           / 
           \/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/ 
-");  
-        }
-        class Program
-        {
-            public void MainMenu()
-            {
-                Banner.Show();   // show your ASCII art first
-                Console.WriteLine("\nPress any key to continue...");
-                Console.ReadKey(true);
-
-                Menu.Show();     // then launch the interactive menu
-            }
-        }
-
+");
     }
+
 }
