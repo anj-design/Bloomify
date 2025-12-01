49a04a9e5cf9190a72fb7d4ac9721f3bb801ec40
aannjjii <aannjjii@DESKTOPLAPPYTAP>
2025-12-01 16:48:49 +0800

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Media;
+
+class TitleScreen
+{
+    public static void Show()
+    {
+        string[] bannerLines = {
+" ___  ___  ___  ___  ___    ____  __     __    __   _  _  __  ____  _  _    ___  ___  ___  ___  ___   ",
+"(___)(___)(___)(___)(___)  (  _ \\(  )   /  \\  /  \\ ( \\/ )(  )(  __)( \\/ )  (___)(___)(___)(___)(___)  ",
+" ___  ___  ___  ___  ___    ) _ (/ (_/\\(  O )(  O )/ \\/ \\ )(  ) _)  )  /    ___  ___  ___  ___  ___   ",
+"(___)(___)(___)(___)(___)  (____/\\____/ \\__/  \\__/ \\_)(_/(__)(__)  (__/    (___)(___)(___)(___)(___)  "
+        };
+
+        Console.Clear();
+        Console.WriteLine(); // one blank line at top
+
+        foreach (var line in bannerLines)
+        {
+            int leftPadding = (Console.WindowWidth - line.Length) / 2;
+            if (leftPadding < 0) leftPadding = 0;
+            Console.WriteLine(new string(' ', leftPadding) + line);
+        }
+
+    }
+}
+
+class Menu
+{
+    public static void Show()
+    {
+        string[] menuItems = { "Start Focus", "History", "Garden" };
+        int selectedIndex = 0;
+
+        while (true)
+        {
+            Console.Clear();
+            TitleScreen.Show();
+            Console.WriteLine("\n\n"); // spacing between banner and menu
+
+            for (int i = 0; i < menuItems.Length; i++)
+            {
+                string line = menuItems[i];
+                int leftPadding = (Console.WindowWidth - line.Length) / 2;
+
+                if (i == selectedIndex)
+                {
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine(new string(' ', leftPadding) + line);
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.WriteLine(new string(' ', leftPadding) + line);ma
+                }
+            }
+
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    selectedIndex = (selectedIndex - 1 + menuItems.Length) % menuItems.Length;
+                    break;
+                case ConsoleKey.DownArrow:
+                    selectedIndex = (selectedIndex + 1) % menuItems.Length;
+                    break;
+                case ConsoleKey.Enter:
+                    Console.Clear();
+                    TitleScreen.Show();
+                    Console.WriteLine($"\nYou selected: {menuItems[selectedIndex]}");
+                    Console.WriteLine("\nPress SPACE to return to menu, or ENTER to exit.");
+
+                    while (true)
+                    {
+                        ConsoleKeyInfo subKey = Console.ReadKey(true);
+                        if (subKey.Key == ConsoleKey.Spacebar)
+                        {
+                            break; // go back to menu loop
+                        }
+                        else if (subKey.Key == ConsoleKey.Enter)
+                        {
+                            Environment.Exit(0); // exit program
+                        }
+                    }
+                    break;
+            }
+
+        }
+    }
+}
---
+++ MainMenu.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Media;
+
+class Banner
+{
+    public static void Show()
+    {
+        Console.WriteLine(@"
+ ___  ___  ___  ___  ___    ____  __     __    __   _  _  __  ____  _  _    ___  ___  ___  ___  ___   
+(___)(___)(___)(___)(___)  (  _ \(  )   /  \  /  \ ( \/ )(  )(  __)( \/ )  (___)(___)(___)(___)(___)  
+ ___  ___  ___  ___  ___    ) _ (/ (_/\(  O )(  O )/ \/ \ )(  ) _)  )  /    ___  ___  ___  ___  ___   
+(___)(___)(___)(___)(___)  (____/\____/ \__/  \__/ \_)(_/(__)(__)  (__/    (___)(___)(___)(___)(___)  
+");
+    }
+}
+
+// === Added code: Menu class ===
+class MainMenu
+{
+    public static void Show()
+    {
+        Console.CursorVisible = false;
+        string[] menuItems = new string[]
+        {
+@"
+ ____  ____  __   ____  ____    ____  __    ___  _  _  ____ 
+/ ___)(_  _)/ _\ (  _ \(_  _)  (  __)/  \  / __)/ )( \/ ___)
+\___ \  )( /    \ )   /  )(     ) _)(  O )( (__ ) \/ (\___ \
+(____/ (__)\_/\_/(__\_) (__)   (__)  \__/  \___)\____/(____/
+",
+@"
+ _  _  __  ____  ____  __  ____  _  _                        
+/ )( \(  )/ ___)(_  _)/  \(  _ \( \/ )                       
+) __ ( )( \___ \  )( (  O ))   / )  /                        
+\_)(_/(__)(____/ (__) \__/(__\_)(__/                         
+",
+@"
+  ___   __   ____  ____  ____  __ _                          
+ / __) / _\ (  _ \(    \(  __)(  ( \                         
+( (_ \/    \ )   / ) D ( ) _) /    /                         
+ \___/\_/\_/(__\_)(____/(____)\_)__)                         
+"
+        };
+
+        int selectedIndex = 0;
+
+        while (true)
+        {
+            Console.Clear();
+            Banner.Show(); // <-- connect: show your banner first
+            Console.WriteLine("\n");
+            DrawMenu(menuItems, selectedIndex);
+
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    selectedIndex = (selectedIndex - 1 + menuItems.Length) % menuItems.Length;
+                    break;
+                case ConsoleKey.DownArrow:
+                    selectedIndex = (selectedIndex + 1) % menuItems.Length;
+                    break;
+                case ConsoleKey.Enter:
+                    Console.Clear();
+                    Console.WriteLine("You selected:\n");
+                    Console.WriteLine(menuItems[selectedIndex]);
+                    Console.WriteLine("\nPress any key to return to menu...");
+                    Console.ReadKey(true);
+                    break;
+            }
+        }
+    }
+
+    static void DrawMenu(string[] items, int selectedIndex)
+    {
+        for (int i = 0; i < items.Length; i++)
+        {
+            if (i == selectedIndex)
+            {
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine(items[i]);
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.WriteLine(items[i]);
+            }
+        }
+    }
+}
+
+// === Entry point ===
